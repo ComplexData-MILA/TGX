f9004c388ae62a812df6c4dbf3ac2db424d35e71
rshirzad <razieh.shirzadkhani@mail.mcgill.ca>
2023-11-06 15:28:25 -0500

    commit before pull

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="py-tgx",
-    version="0.1.2",
+    version="0.1.4",
     description="Temporal graph",
     long_description=readme(),
     long_description_content_type="text/markdown",
