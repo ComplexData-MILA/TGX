e575b1ca319ef1f7da1e22b5bbb3f669a69735d3
Farimah Poursafaei <farimah.poursafaei@gmail.com>
2024-02-19 21:23:33 -0500

    polishing tutorials & setup

--- examples/starting_example.py
+++ examples/starting_example.py
@@ -4,8 +4,8 @@ import argparse
 import sys
 
 def get_args():
-    parser = argparse.ArgumentParser('*** discretizing time steps from datasets ***')
-    parser.add_argument('-d', '--data', type=str, help='Dataset name', default='tgbl-wiki')
+    parser = argparse.ArgumentParser('*** discretizing time steps of a TG dataset ***')
+    parser.add_argument('-d', '--data', type=str, help='dataset name', default='tgbl-wiki')
     parser.add_argument('-t', '--time', type=str, help='time granularity', default='daily')
 
     try:
@@ -18,24 +18,26 @@ def get_args():
 args, _ = get_args()
 
 
-#! load the datasets from tgb or builtin
+# === load the datasets from tgb or built-in ===
+
+# load a built-in dataset
 # dataset = tgx.builtin.uci()
 
-data_name = args.data #"tgbl-coin" #"tgbl-review" #"tgbl-wiki"
+# load a TGB dataset
+data_name = args.data  # args.name can be supported TGB datasets, such as: "tgbl-coin", "tgbl-review", "tgbl-wiki"
 dataset = tgx.tgb_data(data_name)
 
-
-
+# make a Graph object from loaded dataset
 ctdg = tgx.Graph(dataset)
 # ctdg.save2csv("ctdg") #! save the graph to csv files
 
-time_scale = args.time #"minutely"  #"monthly" #"weekly" #"daily"  #"hourly" 
+time_scale = args.time  #choices are: "minutely", "monthly", "weekly", "daily", "hourly" 
 dtdg = ctdg.discretize(time_scale=time_scale)[0]
-print ("discretize to ", time_scale)
+print(f"INFO: Discretize {data_name} to `{time_scale}`")
 
 
 
-#* plotting the statistics
+# === plotting the statistics ===
 tgx.degree_over_time(dtdg, network_name=dataset.name)
 tgx.nodes_over_time(dtdg, network_name=dataset.name)
 tgx.edges_over_time(dtdg, network_name=dataset.name)
@@ -53,7 +55,7 @@ tgx.TEA(dtdg,
 
 
 
-#* compute statistics
+# === compute statistics ===
 test_ratio = 0.15
 tgx.get_reoccurrence(ctdg, test_ratio=test_ratio)
 tgx.get_surprise(ctdg, test_ratio=test_ratio)
